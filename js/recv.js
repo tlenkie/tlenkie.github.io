5ae5a751dfce50f903f9b262ff75b2aeda5e2cd6
Ilya T <baka@derpy.ru>
2018-04-08 21:58:30 +0700

    Add files via upload

--- js/recv.js
+++ js/recv.js
@@ -1,15 +1,15 @@
-var getUrl = function (method, params) {
+function getUrl(method, params) {
 	if (!method) throw new Error('Не указан метод.');
 	params = params || {};
 	params.access_token = access_token;
-	return vk_url + method + '?' $.params(params);
-};
+	return vk_url + method + '?' + $.params(params);
+}
 
-var sendRequest = function (meth, params, func) {
+function sendRequest(meth, params, func) {
 	$.ajax({
 		url: getUrl(meth, params),
 		method: 'GET',
 		dataType: 'JSONP',
 		success: func
 	});
-};
+}
