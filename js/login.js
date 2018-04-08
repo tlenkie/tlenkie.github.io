23488274d0331cc9617f6c684f802d77c36991b3
Ilya T <baka@derpy.ru>
2018-04-08 20:53:21 +0700

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -5,10 +5,11 @@ $(function() {
 	else {
 		$('#login-page').prop('hidden', 'true');
 		$('#main-page').prop('hidden', 'false');
-		access_token = window.location.href.match(/access_token=([^&]+)/)[1];
-		user_id = window.location.href.match(/user_id=([^&]+)/)[1];
-		sendRequest('wall.get', {owner_id: user_id, count: 100}, function(data) {
-			console.log(data);
-		})
-	}	
+		// access_token = window.location.href.match(/access_token=([^&]+)/)[1];
+		// user_id = window.location.href.match(/user_id=([^&]+)/)[1];
+		// sendRequest('wall.get', {owner_id: user_id, count: 100}, function(data) {
+		// 	console.log(data);
+		// })
+		alert(access_token);
+	}		
 });
--- js/variables.js
+++ js/variables.js
@@ -1,3 +1,3 @@
-var access_token = '';
-var user_id = '';
+var access_token = '123';
+var user_id = '345';
 var vk_url = 'https://api.vk.com/method/';
